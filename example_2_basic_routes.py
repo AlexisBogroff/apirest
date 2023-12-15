0dc8ef55d5b81bd29b8a062584cc7a1a462ad0c2
Alexis Bogroff <31806900+AlexisBogroff@users.noreply.github.com>
2023-12-15 03:12:19 +0100

    Add basic routes with parameters

--- example_2_basic_routes.py
+++ example_2_basic_routes.py
@@ -17,9 +17,36 @@ def profile(username):
     return f'{username}\'s profile'
 
 # Route avec paramètre converti (en int)
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return f'Post {post_id}'
+@app.route('/ht_to_ttc/<int:prix_ht>')
+def ht_to_ttc(prix_ht):
+    print(type(prix_ht))
+    return f"Prix TTC: {prix_ht * 1.2}€"
+
+
+# Passage de plusieurs paramètres
+@app.route('/user/<username>/<int:age>')
+def user(username, age):
+    return f'{username} is {age} years old'
+
+
+# Passage de paramètres optionnels
+# Inscrite dans la fonction et non dans la route
+@app.route('/user/optn/<username>/<int:size>/<int:weight>')
+@app.route('/user/optn/<username>/<int:size>')
+def user_properties(username, size, weight=None):
+    if weight:
+        return f'{username} is {size} tall and weighs {weight}'
+    else:
+        return f'{username} is {size} tall'
+
+
+# Passage de paramètres optionnels avec valeur par défaut
+# Inscrite dans la route et non dans la fonction
+@app.route('/blog/page/', defaults={'page': 1})
+@app.route('/blog/page/<int:page>')
+def blog(page):
+    return f'Page du blog {page}'
+
 
 
 if __name__ == "__main__":
