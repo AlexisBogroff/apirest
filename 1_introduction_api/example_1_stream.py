5328835a803e255b57d872473ee67b5cab19ceb8
Alexis Bogroff <31806900+AlexisBogroff@users.noreply.github.com>
2023-12-18 15:10:56 +0100

    Add stream using a generator

--- 1_introduction_api/example_1_stream.py
+++ 1_introduction_api/example_1_stream.py
@@ -1,4 +1,5 @@
 from flask import Flask, stream_with_context, Response
+import time
 
 app = Flask(__name__)
 
@@ -6,12 +7,25 @@ app = Flask(__name__)
 def stream_example():
     def generate():
         yield 'Hello '
+        time.sleep(1)
         yield 'World!'
+        time.sleep(1)
         yield 'This'
+        time.sleep(1)
         yield 'is'
+        time.sleep(1)
         yield 'a'
+        time.sleep(1)
         yield 'stream'
+        time.sleep(1)
         yield 'example.'
     return Response(stream_with_context(generate()))
+
+
+@app.route('/stream2')
+def stream_example2():
+    gen = (i for i in ['Hello ', 'World!', 'This', 'is', 'a', 'stream', 'example.'])
+    return Response(stream_with_context(gen))
+
 
 app.run()
